b288bba3f45d96792e36dfdc5850952305f62024
hameed Kazeem <hameed.kazeem@gmail.com>
2021-02-17 00:25:56 -0500

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # explore-your-city
+
+![GitHub repo size](https://img.shields.io/github/repo-size/hameed1239/explore-your-city?style=plastic)
+![GitHub code size](https://img.shields.io/github/languages/code-size/hameed1239/explore-your-city?style=plastic)
+![GitHub language count](https://img.shields.io/github/languages/count/hameed1239/explore-your-city?style=plastic)
+![GitHub top language](https://img.shields.io/github/languages/top/hameed1239/explore-your-city?style=plastic)
+
+![GitHub last commit](https://img.shields.io/github/last-commit/hameed1239/explore-your-city?style=plastic)
+![GitHub closed pull requests](https://img.shields.io/github/issues-pr-closed-raw/hameed1239/explore-your-city?color=green&style=plastic)
+![GitHub open pull requests](https://img.shields.io/github/issues-pr-raw/hameed1239/explore-your-city?color=red&style=plastic)
+![GitHub closed issues](https://img.shields.io/github/issues-closed-raw/hameed1239/explore-your-city?color=green&style=plastic)
+![GitHub open issues](https://img.shields.io/github/issues-raw/hameed1239/explore-your-city?color=red&style=plastic)
+
+![GitHub stars](https://img.shields.io/github/stars/hameed1239/explore-your-city?style=social)
+![GitHub forks](https://img.shields.io/github/forks/hameed1239/explore-your-city?style=social)
+![GitHub watchers](https://img.shields.io/github/watchers/hameed1239/explore-your-city?style=social)
+![GitHub followers](https://img.shields.io/github/followers/hameed1239?style=social)
+
+[![License: MIT](https://img.shields.io/badge/License-ISC-yellow.svg)](https://opensource.org/licenses/ISC)
+
+## Description
+
+**Adopt Me** is a front-end application for users to get information on points of interests around a given city.
+
+## Table of Contents
+
+- [Deployment URL](#Deployment-URL)
+- [Repo URL](#Repo-URL)
+- [Features](#Features)
+- [Pre-Requisites](#Pre-Requisites)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Screenshots](#Screenshots)
+- [Technologies Used](#Technologies-Used)
+- [Credits](#Credits)
+- [Roadmap](#Roadmap)
+- [Questions](#Questions)
+- [License](#License)
+
+## Deployment URL
+
+https://hameed1239.github.io/explore-your-city/.
+
+## Repo URL
+
+https://github.com/hameed1239/explore-your-city.
+
+## Pre-Requisites
+
+Sign up for an API key from 
+https://opentripmap.io/register
+and
+https://developer.mapquest.com
+
+## Installation
+
+1. Fork the repository
+2. update the API keys with the new ones
+
+## Usage
+
+1. Home page: is the Landing page. When a search is made, the landing page disappears and you're presented with the search result page where new searches can be made
+
+## Screenshots
+
+### Landing Page
+
+![Home Page](assets/images/landing.png)
+
+### Search result Page
+
+![Search result Page](assets/images/search-result.png)
+
+## Technologies Used
+
+This is a front-end application.
+
+1. [HTML5](https://www.w3schools.com/html/)
+1. [CSS3](https://www.w3schools.com/css/)
+1. [JavaScript](https://www.w3schools.com/js)
+1. [JQuery](https://api.jquery.com/)
+1. [OpenTripMap API](https://opentripmap.io/docs)
+1. [MapQuest Open Geocode API](https://www.npmjs.com/package/express)
+1. [Bootstrap](https://getbootstrap.com/docs/5.0/getting-started/introduction/)
+1. [FontAwesome](https://fontawesome.com/)
+1. [FlatIcon](https://www.flaticon.com/)
+1. [Google Fonts](https://fonts.google.com/)
+
+## Collaborators
+
+
+## Roadmap
+
+1. Add a map preview on search results
+
+## Questions
+
+Please send your questions and / or comments to [HAMEED KAZEEM](https://github.com/hameed1239/)  .
+
+## License
+
+This project is licensed under the terms of the [ISC](https://opensource.org/licenses/ISC) license.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,3 @@
-
 let searchBtn = $("#search-btn");
 let coordinates;
 let fetchInterval;
@@ -6,24 +5,12 @@ let counter = 0;
 let placesCategories = ["sport", "accomodations","museums","historical_places","archaeology","theatres_and_entertainments", "beaches", "nature_reserves", "water", "restaurants", "pubs", "islands", "religion", "architecture", "amusements", "adult"];
 let i = 0;
 
-// const getCity = () => {
-//     return $("#search-input").val()
-// }
-
 let getCoodinates = async (city) => {
     fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=eEZcngxNkDzmZxnzJSAyPekHTPjmQyaG&inFormat=kvp&outFormat=json&location=${city}&thumbMaps=false`)
-    // fetch(`https://api.opentripmap.com/0.1/en/places/geoname?name=${city}&apikey=5ae2e3f221c38a28845f05b60fec7edd6c3842c3f334aa9d51ab3bfd`)
         .then(response => {
             return response.json();
         })
         .then(response => {
-            // console.log(response);
-            // if (response.status === "NOT_FOUND") {
-            //     // display a message letting the user know that the city cannot be found
-            //     coordinates = "NOT_FOUND"
-            //     // return;
-            // }
-            // else {
                 $(".result-grid").empty();
                 console.log(response.results[0].locations[0].latLng)
                 fetchInterval = setInterval(() => {
@@ -46,9 +33,12 @@ let getCoodinates = async (city) => {
 
 const getPlacesXID = (coordinates) => {
     // define the categories of places you want in the result
-    if (i === placesCategories.length-1) { clearInterval(fetchInterval) }
+    if (i === placesCategories.length-1) {
+         clearInterval(fetchInterval) 
+         $(".search").removeAttr( "disabled", false );
+        }
 
-    fetch(`https://api.opentripmap.com/0.1/en/places/radius?radius=16093.4&lon=${coordinates.lon}&lat=${coordinates.lat}&rate=2&kinds=${placesCategories[i]}&limit=5&apikey=5ae2e3f221c38a28845f05b60fec7edd6c3842c3f334aa9d51ab3bfd`)
+    fetch(`https://api.opentripmap.com/0.1/en/places/radius?radius=10009.096&lon=${coordinates.lon}&lat=${coordinates.lat}&rate=2&kinds=${placesCategories[i]}&limit=8&apikey=5ae2e3f221c38a28845f05b60fec7edd6c3842c3f334aa9d51ab3bfd`)
         .then(response => {
             return response.json()
         }).then(response => {
@@ -88,19 +78,17 @@ function displayCards(response) {
             let pxIndex = imgSrc.lastIndexOf("px");
             let newImgSrc = imgSrc.replace(`${imgSrc[pxIndex - 3]}${imgSrc[pxIndex - 2]}${imgSrc[pxIndex - 1]}`, "1920");
             let address = response.address;
-            var resultCard = $(`<div class="result"><div class="image-container"><img src=${newImgSrc}  class="result-images" alt=${response.name} onerror="changeImageSrc(this)"></div><div class="result-text"><h2>${response.name}</h2><p>${response.wikipedia_extracts.text}</p><h3>Address</h3><p class="address">${address.house_number || ""} ${address.road || ""} ${address.city ||""} ${address.state||""}  ${address.postcode||""}</p></div></div>`)
+            var resultCard = $(`<div class="result"><div class="image-container"><img src=${newImgSrc}  class="result-images" alt=${response.name} onerror="changeImageSrc(this)"></div><div class="result-text"><h2>${response.name}</h2><p>${response.wikipedia_extracts.text}</p><p class="address">${address.house_number || ""} ${address.road || ""} ${address.city ||""} ${address.state||""}  ${address.postcode||""}</p></div></div>`)
             $(".result-grid").append(resultCard);
             $(".landing").attr("hidden", true)
             $("nav").attr("hidden", false);
-            // if (index === 0) {
-            //     $(".carousel-item").addClass("active");
-            // }
         }
 
     });
-    // fetchAirports();
-    // weatherFetch();
-}// display carousel
+    if(!!$(".result")&&i===placesCategories.length){
+        $(".bg-text").append(`<p class="error">No result was found for this location. Please try again</p>`)
+    }
+}
 
 // Remove image if there's an error when downloading
 const changeImageSrc=(img)=>{
@@ -109,24 +97,17 @@ const changeImageSrc=(img)=>{
 }
 
 $(".search-form").on("submit", (event) => {
-    i=0
     event.preventDefault();
+    $(".error").remove()
+    $(".search").prop( "disabled", true );
+    i=0
     // console.log(event)
     let city = event.target[1].value;
-
+    
     if (city.length) {
         console.log(city)
         getCoodinates(city)
-        // .then(()=>{
-        //     console.log (coordinates)
-        //     getPlacesXID(coordinates)
-        // }
-        // )
-
+            
+        
     }
-})
-
-$(".address").click (function(event) {
-    console.log(event)
-    alert("Hi")
 })
